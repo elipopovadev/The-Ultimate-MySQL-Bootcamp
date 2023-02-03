a72b07f9c8f48e54f3f284a25f24724c9fa04d54
Elena Popova <elipopovadev@gmail.com>
2023-02-03 13:24:09 +0200

    String Functions Challenge

---
+++ String Functions/String Functions Challenge.sql
@@ -0,0 +1,24 @@
+USE book_shop;
+SELECT * FROM books;
+
+SELECT REPLACE(title, ' ', '->') AS 'title'
+FROM books;
+
+SELECT author_lname AS 'forwards',
+       REVERSE(author_lname) AS 'backwards'
+FROM books;
+
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full_name'
+FROM books;
+
+SELECT CONCAT(title, ' was released in ', released_year) AS 'blurb'
+FROM books;
+
+SELECT title, CHAR_LENGTH(title) AS 'char_count'
+FROM books;
+
+SELECT CONCAT(LEFT(title, 10), '...') AS 'short_title',
+       CONCAT(author_lname, ', ', author_fname) AS 'author',
+	   CONCAT(stock_quantity, ' ', 'is stock') AS 'quantity'
+FROM books;
+      

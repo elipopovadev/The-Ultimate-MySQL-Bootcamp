b13f79ce3c29236a19298cdf32f2288da4bf6f0b
Elena Popova <elipopovadev@gmail.com>
2023-02-04 18:32:30 +0200

    Update Aggregate Functions Challenge.sql

--- Aggregate Functions/Aggregate Functions Challenge.sql
+++ Aggregate Functions/Aggregate Functions Challenge.sql
@@ -15,14 +15,14 @@ SELECT SUM(stock_quantity) AS total_number
 FROM books;
 
 -- 4. Find the average released year for each author:
-SELECT CONCAT(author_fname, ' ', author_lname) as 'author',
+SELECT CONCAT(author_fname, ' ', author_lname) AS 'author',
        AVG(released_year) AS 'average_released_year'
 FROM books
 GROUP BY author
 ORDER BY author;
 
 -- 5. Find the full name of the author who wrote thw longest book:
-SELECT CONCAT(author_fname, ' ', author_lname) as 'author',
+SELECT CONCAT(author_fname, ' ', author_lname) AS 'author',
        pages
 FROM books
 WHERE pages = (SELECT MAX(pages)

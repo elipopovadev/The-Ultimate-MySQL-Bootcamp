e0778d4fc5a05bce4d9d8fb8e69ed9725fc2f2d4
Elena Popova <elipopovadev@gmail.com>
2023-02-10 16:49:49 +0200

    Views Challenge

---
+++ Views & Window Functions/Views Challenge.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE office_db;
+USE office_db;
+
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY AUTO_INCREMENT,
+    department VARCHAR(20),
+    salary INT
+);
+ 
+INSERT INTO employees (department, salary) VALUES
+('engineering', 80000),
+('engineering', 69000),
+('engineering', 70000),
+('engineering', 103000),
+('engineering', 67000),
+('engineering', 89000),
+('engineering', 91000),
+('sales', 59000),
+('sales', 70000),
+('sales', 159000),
+('sales', 72000),
+('sales', 60000),
+('sales', 61000),
+('sales', 61000),
+('customer service', 38000),
+('customer service', 45000),
+('customer service', 61000),
+('customer service', 40000),
+('customer service', 31000),
+('customer service', 56000),
+('customer service', 55000);
+
+
+SELECT * FROM employees;
+
+CREATE VIEW min_max_avg_salary_department AS
+SELECT department,
+       MIN(salary) AS min_salary,
+       MAX(salary) AS max_salary,
+       AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department
+ORDER BY avg_salary DESC;
+ 
+ SELECT * FROM min_max_avg_salary_department;
+ 
+ ALTER VIEW min_max_avg_salary_department AS
+ SELECT department,
+       MIN(salary) AS min_salary,
+       MAX(salary) AS max_salary,
+       AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department
+ORDER BY max_salary;
+
+SELECT * FROM min_max_avg_salary_department;
+ 
